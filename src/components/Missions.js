0518bd4971e3c108a47d31f25ba7c5c0161e47b7
Flávio Félix <fau_felix30@hotmail.com>
2023-03-01 22:05:42 -0300

    Feito o requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        oi
+      </div>
+    );
+  }
+}
+
+export default Missions;
